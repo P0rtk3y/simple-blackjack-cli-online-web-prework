60d99370fc639d630cc51f88f2b65e5b05271f9e
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-03 20:05:35 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,7 +23,8 @@ def end_game(card_total)
 end
 
 def initial_round
-  deal_card + deal_card = card_total # code #initial_round here
+  deal_card # code #initial_round here
+  deal_card 
   display_card_total(card_total)
 end
 
